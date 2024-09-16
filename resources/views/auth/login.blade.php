3ef5d0a3b0b65e3a5355a51846bb5ea01c724f00
Annisa Baizan <annisabaizan1@gmail.com>
2024-09-17 05:58:24 +0700

    Benerin Visi 2024

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,7 +67,7 @@
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden">
                                 <span class="mask bg-gradient-primary opacity-7"><img src="{{ asset('/img/GedungGigi.jpg') }}" class="img-fluid position-absolute top-50 start-50 translate-middle"></span>
                                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Visi"</h4>
-                                <p class="text-white position-relative">Menjadi Program Studi yang Menghasilkan Ahli Madya Kesehatan Gigi yang Bermartabat dengan Keunggulan di Bidang Pelayanan Asuhan Kesehatan Gigi dan Mulut Berbsis IT Tahun 2025</p>
+                                <p class="text-white position-relative">Menjadi Program Studi yang Menghasilkan Ahli Madya Kesehatan Gigi yang Bermartabat dengan Keunggulan di Bidang Pelayanan Asuhan Kesehatan Gigi dan Mulut Berbsis IT Tahun 2024</p>
                             </div>
                         </div>
 
