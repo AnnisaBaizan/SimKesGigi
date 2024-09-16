ef4417b24e5e14292fa9b2cd04469dda17b8938b
Annisa Baizan <annisabaizan1@gmail.com>
2024-09-17 06:00:10 +0700

    Diagnosa Keperawatan Gigi

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -165,7 +165,7 @@
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-single-copy-04 text-success text-sm opacity-10"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Diagnosis</span>
+                    <span class="nav-link-text ms-1">Diagnosa Keperawatan Gigi</span>
                 </a>
             </li>
             
